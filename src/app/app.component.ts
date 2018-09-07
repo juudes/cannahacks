4f2524dcf8c54ae9e0a4dea1c4ccc50387113b36
judy <judy@users-MacBook-Pro.local>
2018-09-07 10:43:55 -0400

    hack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weed-em-and-reap';
+  yield: number;
+  waste: number;
+  mother: number;
+  room: number;
+  propagation: number;
+
+
+  calcWaste(room, propagation, mother) {
+    this.yield = 34.6485141 * room - 3.80823765 * propagation - 0.00233187695 * mother + 193589085.81485766;
+  }
+
+  calcYield(room, propagation, mother) {
+    this.waste = 6.63795627 * room + 405.122650 * propagation - 0.00494597731 * mother + 410607461.33089596;
+  }
+
+  onClick() {
+    this.calcWaste(this.room, this.propagation, this.mother);
+    this.calcYield(this.room, this.propagation, this.mother);
+
+  }
 }
